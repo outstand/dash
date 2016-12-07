d7f82642bbe692161ce4d717a2dc7a95a32d1f99
Ryan Schlesinger <ryan@ryanschlesinger.com>
2016-12-06 16:39:14 -0800

    Add ssh hardening

--- docker/rancher/start.sh
+++ docker/rancher/start.sh
@@ -10,3 +10,22 @@ iptables -t nat -A OUTPUT -o lo -p tcp -m tcp --dport 8500 -j DNAT --to ${INSTAN
 # mount --bind /Users /Users
 # mount --make-shared /Users
 mkdir -p /usr/src
+
+cat > /etc/ssh/sshd_config <<EOM
+AuthorizedKeysFile .ssh/authorized_keys
+Subsystem sftp /usr/libexec/sftp-server
+UseDNS no
+PermitRootLogin no
+ServerKeyBits 2048
+AllowGroups docker
+
+KexAlgorithms curve25519-sha256@libssh.org
+Protocol 2
+HostKey /etc/ssh/ssh_host_ed25519_key
+HostKey /etc/ssh/ssh_host_rsa_key
+PasswordAuthentication no
+ChallengeResponseAuthentication no
+PubkeyAuthentication yes
+Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr
+MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,hmac-ripemd160-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,hmac-ripemd160,umac-128@openssh.com
+EOM
