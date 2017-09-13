5e0a44cc7f709a81404f2a81b491084ab2815e89
Ryan Schlesinger <ryan@ryanschlesinger.com>
2017-09-12 17:05:46 -0700

    Rollback to rancherOS 1.0.4

--- docker/provision.sh
+++ docker/provision.sh
@@ -48,7 +48,9 @@ fi
 ros config set rancher.environment.INSTANCE_IP $(ip -o -4 addr show eth0 | awk '{print $4}' | cut -d/ -f1)
 
 ros config set rancher.docker.storage_driver overlay2
-ros engine enable docker-17.06.1-ce
+# 17.06.1 is only available on rancherOS 1.1.0 and up
+# ros engine enable docker-17.06.1-ce
+ros engine enable docker-17.06.0-ce
 
 ros service enable kernel-headers
 ros service enable /var/lib/rancher/conf/consul-$MODE.yml
