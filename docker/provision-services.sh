a80eb0fd0af8115808fcf9466b04ce9e8abf243d
Ryan Schlesinger <ryan@ryanschlesinger.com>
2018-05-31 14:28:19 -0700

    Work around start up issues

--- docker/provision-services.sh
+++ docker/provision-services.sh
@@ -4,9 +4,12 @@ set -ex
 MODE=$1
 
 ros service enable kernel-headers
+ros service up kernel-headers
 ros service enable /var/lib/rancher/conf/consul-$MODE.yml
 ros service enable /var/lib/rancher/conf/schmooze.yml
 ros service enable /var/lib/rancher/conf/dns.yml
+# Work around weird ros bug
+docker start dns
 ros service enable /var/lib/rancher/conf/consul_stockpile.yml
 #ros service enable /var/lib/rancher/conf/nomad-$MODE.yml
 ros service enable /var/lib/rancher/conf/parallels-tools.yml
