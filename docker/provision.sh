83bf9885d1b7bf4cf04273d393c9ec5b77a1e7c8
Ryan Schlesinger <ryan@ryanschlesinger.com>
2019-05-29 12:48:50 -0700

    Upgrade docker to 18.09.4

--- docker/provision.sh
+++ docker/provision.sh
@@ -54,5 +54,5 @@ fi
 
 ros config set rancher.docker.storage_driver overlay2
 sed -ie 's/storage-driver overlay/storage-driver overlay2/' /var/lib/rancher/conf/docker
-ros engine enable docker-18.09.2
+ros engine enable docker-18.09.4
 # storage_driver changes and `engine enable` require an immediate reboot!
